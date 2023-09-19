69ac5f0a4f175263a0e385a97c23104074bd0320
Shrey <80510430+miqumi@users.noreply.github.com>
2023-09-19 12:39:29 +0530

    proj/mveb directory

--- proj/mveb/config.js
+++
@@ -1,5 +0,0 @@
-window.__CONFIG__ = {
-  // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "https://movie-web.rightshreyanshnarwe.workers.dev",
-  VITE_TMDB_READ_API_KEY: "25f000e72d69c829ff0a58f118531e59"
-};
