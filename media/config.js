d328db0b5b74977af8073073c8481e536c727a9b
Shrey <80510430+miqumi@users.noreply.github.com>
2023-10-21 13:29:43 +0530

    testfile

---
+++ media/config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movie-web.rightshreyanshnarwe.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNWYwMDBlNzJkNjljODI5ZmYwYTU4ZjExODUzMWU1OSIsInN1YiI6IjY1MDk0NjZjODI2MWVlMDBjNWZlNWNhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VnNea27xG92KZ8ytzx__kUyB25nQYq6arTmSNGQEGcI"
+};
