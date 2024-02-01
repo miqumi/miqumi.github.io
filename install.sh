5da6e03b76ffa7ea0abce2345ac085659630fe5e
Shrey <80510430+miqumi@users.noreply.github.com>
2024-02-01 19:01:50 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+termux-setup-storage
+pkg update
+pkg install python
+pkg update
+pkg install ffmpeg
+pkg update
+pkg install wget
+pkg update
+pkg install neofetch
+pkg update
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+chmod +rwx yt-dlp
+wget miqumi.github.io/flip.sh
+wget miqumi.github.io/profile
+mv profile .profile
+exit
